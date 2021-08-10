dbdd236273eae9c9b8fa35a3d6785f3f850fff89
김상현 <llmm030506@naver.com>
2021-08-10 23:50:47 +0900

    Initial commit

---
+++ src/main/java/com/java/skanow/SkaNowApplication.java
@@ -0,0 +1,13 @@
+package com.java.skanow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkaNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/skanow/SkaNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.skanow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
