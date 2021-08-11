6b2bace3c3414c2f27df7f5a318df59090c6c842
김상현 <llmm030506@naver.com>
2021-08-12 02:59:35 +0900

    exception en_ko setting

--- src/main/java/com/java/skanow/advice/ExceptionAdvice.java
+++ src/main/java/com/java/skanow/advice/ExceptionAdvice.java
@@ -3,6 +3,8 @@ package com.java.skanow.advice;
 import com.java.skanow.util.response.domain.CommonResult;
 import com.java.skanow.util.response.service.ResponseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,10 +17,22 @@ import javax.servlet.http.HttpServletRequest;
 public class ExceptionAdvice {
 
     private final ResponseService responseService;
+    private final MessageSource messageSource;
+
+    // code정보에 해당하는 메시지를 조회합니다.
+    private String getMessage(String code) {
+        return getMessage(code, null);
+    }
+    // code정보, 추가 argument로 현재 locale에 맞는 메시지를 조회합니다.
+    private String getMessage(String code, Object[] args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-        return responseService.getFailResult();
+        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
+        return responseService.getFailResult(Integer.parseInt(getMessage("unKnown.code")), getMessage("unKnown.msg"));
     }
+
 }
--- src/main/java/com/java/skanow/util/response/service/ResponseService.java
+++ src/main/java/com/java/skanow/util/response/service/ResponseService.java
@@ -58,4 +58,11 @@ public class ResponseService {
         result.setCode(CommonResponse.SUCCESS.getCode());
         result.setMsg(CommonResponse.SUCCESS.getMsg());
     }
+    public CommonResult getFailResult(int code, String msg) {
+        CommonResult result = new CommonResult();
+        result.setSuccess(false);
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
 }
