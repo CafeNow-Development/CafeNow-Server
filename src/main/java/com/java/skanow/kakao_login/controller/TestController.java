b30e95a8fe6f858ad82a3a992f64c1b9f1f53b86
김상현 <llmm030506@naver.com>
2021-08-12 02:51:58 +0900

    api response setting

--- src/main/java/com/java/skanow/kakao_login/controller/TestController.java
+++ src/main/java/com/java/skanow/kakao_login/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.java.skanow.kakao_login.controller;
 
+import com.java.skanow.kakao_login.domain.User;
+import com.java.skanow.util.response.domain.SingleResult;
+import com.java.skanow.util.response.service.ResponseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1")
 public class TestController {
 
+    private final ResponseService responseService;
+
     @ApiOperation(value = "테스트", notes = "테스트 home 출력")
     @GetMapping("/test")
     public String home() {
         return "home";
     }
 
+    @ApiOperation(value = "테스트", notes = "테스트 home 출력")
+    @GetMapping("/test2")
+    public SingleResult<User> UserTest() {
+        User user = new User(1L, "llmm030506@gmail.com", "김상현");
+        return responseService.getSingleResult(user);
+    }
 }
